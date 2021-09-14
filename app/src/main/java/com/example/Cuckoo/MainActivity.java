4f7b108762c38d1440f000d98221c4c1621375cd
u7274374 <u7274374@anu.edu.au>
2021-09-15 05:17:32 +0800

    Some update...

--- app/src/main/java/com/example/Cuckoo/MainActivity.java
+++ app/src/main/java/com/example/Cuckoo/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.Cuckoo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.Cuckoo.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Called when the user taps the Send button
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = findViewById(R.id.editTextTextPersonName3);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
