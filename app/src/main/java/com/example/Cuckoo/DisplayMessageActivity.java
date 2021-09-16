3df4f2ffaa920bdb25720420f6686da1939de2d7
u7274374 <u7274374@anu.edu.au>
2021-09-16 20:18:07 +0800

    A test for Layout update.

--- app/src/main/java/com/example/Cuckoo/DisplayMessageActivity.java
+++ app/src/main/java/com/example/Cuckoo/DisplayMessageActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
@@ -24,5 +25,11 @@ public class DisplayMessageActivity extends AppCompatActivity {
         {
             textView.setText("A surprise!");
         }
+    }
+
+    public void enterChatInterface(View view)
+    {
+        Intent intent = new Intent(this, ChatActivity.class);
+        startActivity(intent);
     }
 }
